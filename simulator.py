a64b4fe1b88bf7142fdad839396c544df01c52c1
max.kishkevich <max.kishevich@itibo.com>
2021-11-25 18:34:05 +0300

    Print statistics, considering draws

--- simulator.py
+++ simulator.py
@@ -1,25 +1,31 @@
+from collections import Counter
 from scipy.stats import binom_test
 
 from src.importsTools import import_bot
-
 from playGame import play_one_game
 
 
 def play(bot1, bot2, n_games):
     wins = [0, 0]
     scores = [0, 0]
+    descriptions = Counter()
     for g in range(n_games):
         result = play_one_game(bot1, bot2)
-        wins[result['metadata']['winner'] - 1] += 1
-        scores[0] += result['metadata']['score'][0]
-        scores[1] += result['metadata']['score'][1]
+        for i in range(2):
+            wins[i] += result['metadata']['result'][i]
+            scores[i] += result['metadata']['score'][i]
+        descriptions.update({result['metadata']['description']: 1})
+    n_wins = sum(wins)
     print(f'Total games: {n_games}')
-    print(f'Wins: {wins[0]}:{wins[1]} ({int(wins[0] / n_games * 100)}%)')
+    print(f'Results: +{wins[0]}={n_games - n_wins}-{wins[1]} ({int(wins[0] / n_wins * 100)}%)')
     print(f'Score: {scores[0]}:{scores[1]}')
-    print('P-value: {:.3f}'.format(binom_test(wins[0], n_games)))
+    print('P-value: {:.3f}'.format(binom_test(wins[0], n_wins)))
+
+    for desc in descriptions.most_common():
+        print(f'{desc[1]}: {desc[0]}')
 
 
 random_bot1 = import_bot('random_bot.py', 'random1')
 random_bot2 = import_bot('random_bot.py', 'random2')
 
-play(random_bot1, random_bot2, 100)
+play(random_bot1, random_bot2, 1000)
